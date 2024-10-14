4adc1eb369ec5eb7286d86f00c4b09d81a4f477d
P. Pablo Cubells Talavera <pablocub@ucm.es>
2024-10-14 19:59:19 +0000

    Default mode

---
+++ src/js/component/card.jsx
@@ -0,0 +1,29 @@
+<section class="container bg-white p-5 border-top">
+    <div>
+        <h1>Portfolio</h1>
+        <p class="fs-5">Praesent tellus nisl, ultrices sit amet pharetra nec, blandit a mi. Nullam molestie non tortor laoreet faucibus. Sed varius rhoncus magna vitae viverra. Cras pulvinar porta consectetur. Vivamus at orci tristique, semper erat tincidunt, commodo nunc.</p>
+    </div>
+    <div class="row">
+        <div class="col-lg-4 px-3 mb-4 ">
+            <img src="assets/img/big-project-image.png" class="img-fluid" alt="big-project-image.png"/>
+            <div class="mt-4">
+                <h5 class="fw-bold text-primary">Project Name</h5>
+                <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec porta lectus quis augue scelerisque, at cursus sem fringilla.</p>
+            </div>
+        </div>
+        <div class="col-lg-4 px-3 mb-4 ">
+            <img src="assets/img/big-project-image.png" class="img-fluid" alt="big-project-image.png"/>
+            <div class="mt-4">
+                <h5 class="fw-bold text-primary">Project Name</h5>
+                <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec porta lectus quis augue scelerisque, at cursus sem fringilla.</p>
+            </div>
+        </div>
+        <div class="col-lg-4 px-3 mb-4 ">
+            <img src="assets/img/big-project-image.png" class="img-fluid" alt="big-project-image.png"/>
+            <div class="mt-4">
+                <h5 class="fw-bold text-primary">Project Name</h5>
+                <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec porta lectus quis augue scelerisque, at cursus sem fringilla.</p>
+            </div>
+        </div>
+    </div>
+</section>
