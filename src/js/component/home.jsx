3271b5f9f98c9d0e975360f8844d1ecdaad96a63
P. Pablo Cubells Talavera <pablocub@ucm.es>
2024-10-17 19:28:18 +0000

    Remache final

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -7,26 +7,20 @@ import Stock from "./stock.jsx";
 import Navbar from "./navbar.jsx";
 import Jumbotron from "./jumbotron.jsx";
 
-import rigoImage from "../../img/rigo-baby.jpg";
-// import {buttonsData} from "./raw_data/raw_NavbarData.jsx";
-// import {cardsData} from "./raw_data/raw_CardsData.jsx";
-import {Data} from "./raw_data/raw_Data.jsx";
+import {buttonsData} from "./raw_data/raw_NavbarData.jsx";
+import {cardsData} from "./raw_data/raw_CardsData.jsx";
 const rndText="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
 
 //create your first component
 const Home = () => {
-	// console.log(Data[0].cardsData) === console.log(cardsData)
-	// console.log(Data[1].buttonsData) === console.log(buttonsData)
 	return (
-		// _Default({image: rigoImage})
 		<div>
-			{/* <_Default image={rigoImage} /> */}
-			<Navbar title="Start Bootstrap" buttons={Data[0].buttonsData} />
+			<Navbar title="Start Bootstrap" buttons={buttonsData} />
 			<Jumbotron 
 				title="It's wonderful to see you!!" 
 				jumboText={rndText}
 				buttonText="Click para comenzar!" />
-			<Stock cards={Data[1].cardsData} />
+			<Stock cards={cardsData} />
 			<Footer copyRight="Copyright &copy; 4Geeks Academy ~ Acedpol" />
 		</div>
 	);
